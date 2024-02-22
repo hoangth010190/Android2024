5c56cdd0ed28a7b7841e26434966768bfb764404
PM16-PC-TEACHER\SV <hoangth>
2024-02-22 16:49:20 +0700

    Navigation view

--- app/src/main/java/com/example/facebookfake/HomeActivity.java
+++ app/src/main/java/com/example/facebookfake/HomeActivity.java
@@ -3,6 +3,7 @@ package com.example.facebookfake;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
@@ -12,6 +13,9 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 
 import com.example.facebookfake.fragments.CallFragment;
@@ -20,12 +24,18 @@ import com.example.facebookfake.fragments.PeopleFragment;
 import com.example.facebookfake.fragments.StoriesFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
+import com.google.android.material.navigation.NavigationView;
 
 
 public class HomeActivity extends AppCompatActivity {
     Toolbar myToolbar;
+    DrawerLayout myDrawer;
+    NavigationView myNavigation;
 
-    @SuppressLint("MissingInflatedId")
+    View headerNavigation;
+
+    ImageButton btAvatar, btSetting;
+    Button btName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,6 +45,39 @@ public class HomeActivity extends AppCompatActivity {
         setSupportActionBar(myToolbar);
 
         myToolbar.setTitle("Chats");
+
+        myDrawer  = findViewById(R.id.drawerLayoutHome);
+        myNavigation = findViewById(R.id.navigationViewHome);
+        myNavigation.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int size = myNavigation.getMenu().size();
+                for (int i = 0; i < size; i++) {
+                    myNavigation.getMenu().getItem(i).setChecked(false);
+                }
+
+                if(item.getItemId() == R.id.mnDrawerHomeChat) {
+                    // Do Something
+                }
+
+                if(item.getItemId() == R.id.mnDrawerHomeLogout) {
+                    finish();
+                }
+                item.setChecked(true);
+                myDrawer.close();
+                return true;
+            }
+        });
+
+        headerNavigation = myNavigation.getHeaderView(0);
+        btSetting = headerNavigation.findViewById(R.id.imageButtonSetting);
+        btSetting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(HomeActivity.this, NewChatActivity.class);
+                startActivity(i);
+            }
+        });
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationViewHome);
         bottomNavigationView.setOnItemSelectedListener(onItemSelectedListener());
@@ -61,6 +104,9 @@ public class HomeActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            myDrawer.open();
+        }
         if (item.getItemId() == R.id.mnItemNewMessage) {
             Intent i = new Intent(HomeActivity.this, NewChatActivity.class);
             startActivity(i);
